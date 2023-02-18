5120ea24ad53965a2e51ec18e7c0d35cf6ff62b7
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-02-18 20:26:38 +0530

    my 1st commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -14,16 +14,60 @@ public class Email {
         return emailId;
     }
 
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
     public String getPassword() {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if (password.equals(oldPassword)) {
+            if (isValid(newPassword)) {
+                System.out.println("Your password is updated successfully.");
+            } else {
+                System.out.println("Your password not is valid.");
+            }
+        } else {
+            System.out.println("Your entered password is wrong.");
+        }
+    }
+       private boolean isValid(String password){
+        boolean capitalLetter=false;
+        boolean smallLetter=false;
+       boolean digit=false;
+        boolean speacialCharacter=false;
+
+           if(password.length()<8){
+            return false;
+                }
+           //now check all characters of password
+for(int i=0;i<password.length();i++){
+    char ch =password.charAt(i);
+    if(ch>='A' && ch<='Z'){
+        capitalLetter=true;
+    }
+    else if(ch>='a' && ch<='z'){
+        smallLetter=true;
+    }
+    else if(ch>=0 && ch<='9'){
+        digit=true;
+    }
+    else{
+        speacialCharacter=true;
+    }
+}
+return capitalLetter && smallLetter && digit && speacialCharacter;
     }
 }
--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -3,13 +3,30 @@ package com.driver;
 import java.util.ArrayList;
 import java.util.Date;
 
+class Emailformat{
+    Date date;
+    String sender;
+    String message;
+
+    public Emailformat(Date date, String sender, String message) {
+        this.date = date;
+        this.sender = sender;
+        this.message = message;
+    }
+}
 public class Gmail extends Email {
 
+    ArrayList<Emailformat>Inbox;
+    ArrayList<Emailformat>Trash;
     int inboxCapacity; //maximum number of mails inbox can store
     //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String). It is guaranteed that message is distinct for all mails.
     //Trash: Stores mails. Each mail has date (Date), sender (String), message (String)
     public Gmail(String emailId, int inboxCapacity) {
 
+   super(emailId);
+   this.inboxCapacity=inboxCapacity;
+   Inbox=new ArrayList<>();
+   Trash=new ArrayList<>();
     }
 
     public void receiveMail(Date date, String sender, String message){
@@ -17,49 +34,90 @@ public class Gmail extends Email {
         // It is guaranteed that:
         // 1. Each mail in the inbox is distinct.
         // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
-
+        if(Inbox.size() == inboxCapacity){
+            //Get the firstEmailTemplate
+            Emailformat emailTemplate = Inbox.get(0);
+            Inbox.remove(0); //remove it from Inbox
+            Trash.add(emailTemplate); //add to trash
+        }
+        //add latest email  by creating new object it new email
+        Emailformat emailTemplate = new Emailformat(date, sender , message);
+        Inbox.add(emailTemplate);
     }
 
     public void deleteMail(String message){
         // Each message is distinct
         // If the given message is found in any mail in the inbox, move the mail to trash, else do nothing
-
+        //We have an arrayList of emailTemplate, Find its index : matching message with emailTemplate message
+        Emailformat emailTemplate = null;
+        for(int i=0; i<Inbox.size(); i++){
+            Emailformat emailTemplate1 = Inbox.get(i);
+            if(emailTemplate1.message.equals(message)){
+                emailTemplate = emailTemplate1;
+                break;
+            }
+        }
+        if(emailTemplate != null){
+            Inbox.remove(emailTemplate);
+            Trash.add(emailTemplate);
+        }
     }
 
     public String findLatestMessage(){
         // If the inbox is empty, return null
         // Else, return the message of the latest mail present in the inbox
-
+if(Inbox.isEmpty()){
+    return null;
+}
+else{
+    Emailformat latestemail = Inbox.get(Inbox.size() - 1);
+    return latestemail.message;
+}
     }
 
     public String findOldestMessage(){
         // If the inbox is empty, return null
         // Else, return the message of the oldest mail present in the inbox
-
+if(Inbox.isEmpty()){
+    return null;
+}
+else{
+    Emailformat oldemail = Inbox.get(0);
+    return oldemail.message;
+}
     }
 
     public int findMailsBetweenDates(Date start, Date end){
         //find number of mails in the inbox which are received between given dates
         //It is guaranteed that start date <= end date
-
+        int count = 0;
+        for(int i=0; i<Inbox.size(); i++){
+            Emailformat emails = Inbox.get(i);
+            //Compare the Date
+            if((emails.date.compareTo(start) >= 0) && (emails.date.compareTo(end) <= 0)){
+                count++;
+            }
+        }
+        return count;
     }
 
     public int getInboxSize(){
         // Return number of mails in inbox
-
+return Inbox.size();
     }
 
     public int getTrashSize(){
         // Return number of mails in Trash
-
+return Trash.size();
     }
 
     public void emptyTrash(){
         // clear all mails in the trash
-
+      Trash.clear();
     }
 
     public int getInboxCapacity() {
         // Return the maximum number of mails that can be stored in the inbox
+        return inboxCapacity;
     }
 }
